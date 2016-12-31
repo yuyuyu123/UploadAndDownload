2c27021761689c70b2b97c84416d183864eeffeb
If rest, rust! <996078887@qq.com>
2016-12-31 15:39:56 +0800

    Update RetrofitDownloadService.java

--- download_upload/src/main/java/com/yulin/download_upload/download/RetrofitDownloadService.java
+++ download_upload/src/main/java/com/yulin/download_upload/download/RetrofitDownloadService.java
@@ -12,7 +12,7 @@ import retrofit2.http.Url;
  * Instances of this class should be obtained through
  * {@link com.yulin.download_upload.api.ManagerFactory#getManager(Class)}.
  *
- * @Auther LiuLiWei
+ * @Author YuLin
  */
 public interface RetrofitDownloadService {
 
